2dc186be4917fd39ca6601b84af72f42a575f800
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-29 02:46:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,10 @@ def my_collect(collection)
   i = 0 
   fresh_collection = []
   while i < collection.length 
-  
+  fresh_collection << yield(collection[i])
+  i += 1 
+end
+fresh_collection
   
 end
 
